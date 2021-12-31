24819b8f936912c8a7ee77a996e0ffe6fcfe55cd
eduardoworrel <=>
2021-12-31 18:12:09 -0300

    corrigindo identação

--- src/Palavras.etl/schedule.js
+++ src/Palavras.etl/schedule.js
@@ -1,16 +1,17 @@
-const {extract} = require('./src/Extract/extract');
-const {transform} = require('./src/Transform/transform');
-const {load} = require('./src/Load/load');
+const {extract} = require('./Extract/extract');
+const {transform} = require('./Transform/transform');
+const {load} = require('./Load/load');
 
 const cron = require("node-cron");
 
 async function start(){
     try{
-    await load(await transform(await extract()))
+        await load(await transform(await extract()))
     }
-    catch(e){console.log(e)} 
+    catch(e){
+        console.log(e)
+    } 
 }
-
 cron.schedule("00 00 00,6,12,18 * * *", async () => {
    await start();
 })

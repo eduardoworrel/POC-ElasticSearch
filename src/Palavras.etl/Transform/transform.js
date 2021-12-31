df214448c34dec834b3952b23606e81c8abc0647
eduardoworrel <=>
2021-12-31 14:56:33 -0300

    renomeando diretórios

---
+++ src/Palavras.etl/Transform/transform.js
@@ -0,0 +1,14 @@
+const {blackList} = require('./blacklist.js.js') 
+const whiteSpace = ' ';
+
+module.exports = {
+    transform : (collection) => {
+        //remove pronomes e conectores
+        for(keyValue of collection){
+            for(remove of blackList){
+                keyValue.data = keyValue.data.replace(remove, whiteSpace);
+            }
+        }
+        return collection;
+    }
+}

55de26f52098b21475ea10d72c3e368e169383a7
eduardoworrel <=>
2022-01-02 20:05:14 -0300

    remove alternative sites

--- src/Palavras.Background.ETL/Extract/sitesAction.js
+++ src/Palavras.Background.ETL/Extract/sitesAction.js
@@ -50,52 +50,53 @@ const cnnbrasil = async (page) => {
         data: pageText
     }
 }
-const jovempan = async (page) => {
-    await page.goto("https://jovempan.com.br");
-    const pageText = await page.evaluate(() => {
-        const _document = document;
-        [
-            ".container-top-to-menu-link",
-            "#footer",
-            ".container-panflix-collection",
-            ".podcasts-header-home",
-            ".container-follow-apps"
+// const jovempan = async (page) => {
+//     await page.goto("https://jovempan.com.br");
+//     const pageText = await page.evaluate(() => {
+//         const _document = document;
+//         [
+//             ".container-top-to-menu-link",
+//             "#footer",
+//             ".container-panflix-collection",
+//             ".podcasts-header-home",
+//             ".container-follow-apps"
 
-        ].forEach((selector) => {
-            _document.querySelector(selector).remove()
-        });
-        return _document.querySelector("body").innerText;
-    });
+//         ].forEach((selector) => {
+//             _document.querySelector(selector).remove()
+//         });
+//         return _document.querySelector("body").innerText;
+//     });
 
-    return {
-        key: 'jovempan.com.br',
-        data: pageText
-    }
-}
-const terra = async (page) => {
-    await page.goto("https://www.terra.com.br");
-    const pageText = await page.evaluate(() => {
-        const _document = document;
-            [
-                ".table-ad",
-                "#zaz-app-t360-navbar",
+//     return {
+//         key: 'jovempan.com.br',
+//         data: pageText
+//     }
+// }
+
+// const terra = async (page) => {
+//     await page.goto("https://www.terra.com.br");
+//     const pageText = await page.evaluate(() => {
+//         const _document = document;
+//             [
+//                 ".table-ad",
+//                 "#zaz-app-t360-navbar",
               
-            ].forEach((selector) => {
-                _document.querySelector(selector).remove()
-            });;
+//             ].forEach((selector) => {
+//                 _document.querySelector(selector).remove()
+//             });;
 
-        return _document.querySelector("body").innerText;
-    });
+//         return _document.querySelector("body").innerText;
+//     });
 
-    return {
-        key: 'terra.com.br',
-        data: pageText
-    }
-}
+//     return {
+//         key: 'terra.com.br',
+//         data: pageText
+//     }
+// }
 module.exports = {
     globo,
     uol,
     cnnbrasil,
-    jovempan,
-    terra
+    // jovempan,
+    //terra
 }

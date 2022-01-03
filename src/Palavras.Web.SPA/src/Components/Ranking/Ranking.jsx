c23b1fcc90765ac86a8e8eb1beb2e57afb4cffa4
eduardoworrel <=>
2022-01-02 22:48:22 -0300

    incluindo rank por site e estilizando

--- src/Palavras.Web.SPA/src/Components/Ranking/Ranking.jsx
+++ src/Palavras.Web.SPA/src/Components/Ranking/Ranking.jsx
@@ -1,37 +1,75 @@
-import { Heading, Box, List } from "@dracula/dracula-ui";
+import { Heading, Box, List, Card, Text } from "@dracula/dracula-ui";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobeAmericas, faCube } from '@fortawesome/free-solid-svg-icons'
 
+const UrlRank = "http://localhost:5001/Page/GetRank"
+const UrlGroup = "http://localhost:5001/Page/GetGroups"
+const Ranking = () => {
+    const [list, setList] = useState([]);
+    const [group, setGroup] = useState([]);
 
-const Url = "http://localhost:5001/Page/GetRank"
-const Ranking = ()=>{
-    const [list,setList] = useState([]);
-
-    const load = ()=>{
-        fetch(Url).then(a => a.json()).then((a)=>{
+    const load = () => {
+        fetch(UrlRank).then(a => a.json()).then((a) => {
             setList(a)
         });
+        fetch(UrlGroup).then(a => a.json()).then((a) => {
+            setGroup(a)
+        });
     }
-    useEffect(load,[]);
-    
+    useEffect(load, []);
+
     return (
-        
+
         <Box>
-            <Heading p="sm">
-                TOP 10 PALAVRAS 
-            </Heading>
-           
-            {list.map((i,c)=> 
-            <div key={c}>
-                <List size="lg" p="md">
-                <li className="drac-text drac-text-white"><b>{c + 1}º</b>
-                - {i.word} </li>
-                
-                <div className="bag">
-                    {i.count} 
-                </div>
-                </List>
-            </div>
-            )}
+            <Heading size="xl" style={{
+                textAlign:"center",
+                marginTop:"50px",
+                marginBottom:"50px"
+            }}>Palavras mais citadas nos sites de notícia</Heading>
+            <Card style={{
+                width: "22%",
+                float: "left"
+            }}
+                color="purpleCyan" m="sm" p="sm">
+                <Heading p="sm" >
+                    <Text color="black"><FontAwesomeIcon icon={faGlobeAmericas} />   TOP 10 </Text>
+                </Heading>
+                {list.map((i, c) =>
+                    <div key={c}>
+                        <List size="" p="">
+                            <li className="drac-text drac-text-white">
+                                <Text color="black"> {c + 1}º <b>{i.word}</b> ({i.count})</Text>
+                            </li>
+                        </List>
+                    </div>
+                )}
+            </Card>
+
+       
+                {group.map((i, c) =>
+                        <Card style={{
+                            width: "22%",
+                            float: "left"
+                        }}
+                            color="pinkPurple" m="sm" p="sm">
+            
+                   
+                        <div key={c}>
+                            <Heading p="sm" size="sm">
+                            <Text color="black"> <FontAwesomeIcon icon={faCube} /> {i.site} </Text>
+                            </Heading>
+                            <List size="" p="">
+                                {
+                                    i.wordCounts.map((wordCount, count) =>
+                                        <li key={count} className="drac-text drac-text-white">
+                                             <Text color="black"> {count + 1}º <b>{wordCount.word}</b> ({wordCount.count})</Text>
+                    </li>
+                                    )}
+                            </List>
+                        </div>
+                        </Card>
+                )}
            
         </Box>
     )

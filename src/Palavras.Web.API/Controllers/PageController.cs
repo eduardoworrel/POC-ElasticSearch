646fdb74b7febda52a371792bde7f3d6180bc8fa
eduardoworrel <=>
2022-01-05 00:03:06 -0300

    usings

--- src/Palavras.Web.API/Controllers/PageController.cs
+++ src/Palavras.Web.API/Controllers/PageController.cs
@@ -1,4 +1,10 @@
 using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Nest;
 namespace Api.Controllers
 {
 

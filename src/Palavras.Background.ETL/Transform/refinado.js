8d4c5966ec183a487cfb868370d100499ebeb96b
Eduardo Lima De Almeida Worrel <eduardo.worrel@gmail.com>
2022-01-05 23:52:30 -0300

    consulta dicionario para filtrar por classe gramatical

--- src/Palavras.Background.ETL/Transform/refinado.js
+++ src/Palavras.Background.ETL/Transform/refinado.js
@@ -1,22 +1,49 @@
 const whiteSpace = ' ';
 const url = 'https://significado.herokuapp.com/';
+function groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach((item) => {
+         const key = keyGetter(item);
+         const collection = map.get(key);
+         if (!collection) {
+             map.set(key, [item]);
+         } else {
+             collection.push(item);
+         }
+    });
+    return map;
+}
+
 module.exports = {
     transform : (collection) => {
         //remove pronomes e conectores 
-        cleanList = [];
+        WordList = [];
         for(keyValue of collection){
-            let newData = keyValue.data.toLowerCase();
-            for(remove of sinalList){
-                newData = newData.replace(remove, whiteSpace)
-            }
-            for(remove of blackList){
-                newData = newData.replace(new RegExp(remove, 'g'), whiteSpace)
-            }
-            cleanList.push({
-                key:   keyValue.key,
-                data:  newData.trim()
+
+            let newData = keyValue.data.toUpperCase();
+            let splitData = newData.split(whiteSpace);
+            splitData = splitData.filter(function(el){
+                return el != '';
             });
+            const group = groupBy(splitData, (item) => item);
+
+            for(wordArray of group){
+                let urlRequest = url + wordArray[0];
+                let response = fetch(urlRequest).then(res => res.json());
+                response.then(data => {
+                    if(!data.error){
+                        let newWord = {
+                            site : keyValue.Key,
+                            word : wordArray[0],
+                            class : data.class,
+                            count : wordArray.length,
+                        }
+                        WordList.push(newWord);
+                    }
+                });
+            }
+          
         }
-        return cleanList;
+        return WordList;
     }
 }

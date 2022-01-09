6f0e832c03f3eb52b72f242baba514c74ed50195
eduardoworrel <=>
2022-01-09 17:32:51 -0300

    otimizando layout

--- src/Palavras.Web.SPA/src/Components/Ranking/Ranking.jsx
+++ src/Palavras.Web.SPA/src/Components/Ranking/Ranking.jsx
@@ -1,7 +1,7 @@
-import { Heading, Box, List, Card, Text, Paragraph, Divider } from "@dracula/dracula-ui";
+import { Heading, Box, Button, Card, Text, Paragraph, Divider } from "@dracula/dracula-ui";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobeAmericas, faCube, faCubes } from '@fortawesome/free-solid-svg-icons'
+import { faGlobeAmericas, faCube, faFilter, faLock } from '@fortawesome/free-solid-svg-icons'
 import { Puff } from 'react-loading-icons'
 import { PolarAreaWithoutLegend } from "../Chart/PolarAreaWithoutLegend";
 
@@ -10,11 +10,13 @@ import env from "react-dotenv";
 const UrlUltimaAtualizacao = env.API + "/PageWord/GetUltimaAtualizacao"
 const UrlRank = env.API + "/PageWord/GetRank"
 const UrlGroup = env.API + "/PageWord/GetGroups"
+const UrlClasses = env.API + "/PageWord/GetClasses"
 
 const Ranking = () => {
     const [list, setList] = useState([]);
     const [group, setGroup] = useState([]);
     const [atualizacao, setAtualizacao] = useState("");
+    const [classes, setClasses] = useState([]);
 
     const load = () => {
         fetch(UrlRank).then(a => a.json()).then((a) => {
@@ -26,92 +28,148 @@ const Ranking = () => {
         fetch(UrlUltimaAtualizacao).then(a => a.text()).then((a) => {
             setAtualizacao(a)
         });
+        fetch(UrlClasses).then(a => a.json()).then((a) => {
+            setClasses(a)
+        });
     }
     useEffect(load, []);
 
     return (
         <>
-            <Box>
-                <Heading size="xl" style={{
-                    textAlign: "center",
-                    marginTop: "50px",
-                    marginBottom: "50px"
-                }}>Palavras mais citadas nos sites de notícia</Heading>
-                <Card className="smallCard"
-                    variant="subtle"
-                    color="pink" m="sm" p="sm">
+            <Box style={{ width: "95%", margin: "0 auto" }}>
 
-                    <div>
-                        <Heading p="sm" size="sm">
-                            <Text color="white"> <FontAwesomeIcon icon={faCube} /> Ultima Atualização</Text>
-                        </Heading>
-                        {atualizacao ?
-                            <Paragraph>
-                                <Text color="white"> <b>{atualizacao}</b> </Text>
-                            </Paragraph>
-                            : <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>}
-                    </div>
-                </Card>
-                {group.map((i, c) =>
-                    <Card key={c} className="smallCard"
-                        variant="subtle" color="pinkPurple" m="sm" p="sm">
-                        <div >
-                            <Heading p="sm" size="sm">
-                                <Text> <FontAwesomeIcon icon={faCube} /> {i.site} </Text>
-                            </Heading>
-                            <Box>
-                                {
-                                    i.palavras.map((wordCount, count) =>
-                                        <div key={count} className="drac-text drac-text-white">
-                                            <Text> {count + 1}º <b>{wordCount.palavra}
-                                            </b> ({((wordCount.frequencia / i.palavras
-                                                .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)}%)</Text>
-                                        </div>
-                                    )
-                                }
-                            </Box>
-                        </div>
-                    </Card>
-                )}
+                <Box>
+                    <Heading size="xl" style={{
+                        textAlign: "center",
+                        marginTop: "50px",
+                        marginBottom: "50px"
+                    }}>Palavras mais citadas nos sites de notícia</Heading>
+                </Box>
 
-            </Box>
-            <Box>
 
-                <Card className="smallCard"
-                    variant="subtle" color="pink" m="sm" p="sm">
-                    <Heading p="sm" >
-                        <Text><FontAwesomeIcon icon={faGlobeAmericas} /> TOP 10 </Text>
-                    </Heading>
-                    {list.length ?
-                        <>
-                            <List size="" p="">
-                                {list.map((i, c) =>
-                                    <li key={c} className="drac-text drac-text-white">
-                                        <Text> {c + 1}º <b>{i.palavra}</b>
+                <Heading size="xl">
+                    <FontAwesomeIcon icon={faFilter} /> CLASSES GRAMATICAIS
+                </Heading>
+                <Divider color="purple" />
+
+                <Box style={{ display: "flex", flexFlow: "row wrap", width: "95%", margin: "0 auto" }}>
+                    <>
+                        {classes.length > 0 ?
+                            classes.map((classe, k) =>
+                                <>
+                                    {classe == "?" ?
+                                        <Box style={{ order: "1", margin: "auto" }} key={k} m="">
+                                            <Button m="xxs" color="purple" disabled={true} >
+                                                DESCONHECIDO  <FontAwesomeIcon icon={faLock} />
+                                            </Button>
+                                        </Box>
+                                        :
+                                        <Box style={{ order: "2", margin: "auto" }} key={k} m="">
+                                            <Button m="xxs" color="purple" variant="ghost">
+                                                {classe}
+                                            </Button>
+                                        </Box>
+                                    }
+                                </>
+                            )
+                            :
+                            <><Puff stroke="cyan" strokeOpacity={.925} speed={.75} /></>
+                        }
+                    </>
+                </Box>
+
+                <Heading size="xl">
+                    <FontAwesomeIcon icon={faGlobeAmericas} /> TOP 10
+                </Heading>
+                <Divider color="pink" />
+                <Box>
+                    <Box className="smallCard">
+                        <Card
+                            variant="subtle" color="pink" m="sm" p="sm">
+                            {list.length ?
+                                <>
+
+                                    <Box style={{ padding: "0 5%" }}>
+                                        {list.map((i, c) =>
+                                            <Box key={c}>
+                                                <Text> {c + 1}º <b>{i.palavra}</b>
                                     ({(
-                                                (i.frequencia / list
-                                                    .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)
-                                            }%)</Text>
-                                    </li>
-                                )}
-                            </List>
-                        </>
-                        :
-                        <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>}
-                </Card>
+                                                        (i.frequencia / list
+                                                            .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)
+                                                    }%)</Text>
+                                                <br />
+                                            </Box>
 
-                <Card className="bigCard"
-                    variant="subtle" color="pink" m="sm" >
-                    <Heading p="sm" >
-                        <Text><FontAwesomeIcon icon={faGlobeAmericas} /> TOP 10 </Text>
+                                        )}
+                                    </Box>
+                                </>
+                                :
+                                <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>}
+                        </Card>
+                        <Card
+                            variant="subtle"
+                            color="pink" m="sm" p="sm">
+
+                            <div>
+                                <Heading p="sm" size="sm">
+                                    <Text color="white"> <FontAwesomeIcon icon={faCube} /> Ultima Atualização</Text>
+                                </Heading>
+
+                                <Box style={{ padding: "0 7%" }}>
+                                    {atualizacao ?
+                                        <Paragraph>
+                                            <Text color="white"> <b>{atualizacao}</b> </Text>
+                                        </Paragraph>
+                                        : <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>}
+                                </Box>
+                            </div>
+                        </Card>
+                    </Box>
+
+                    <Card className="bigCard"
+                        variant="subtle" color="pink" m="sm" >
+
+                        {list.length ?
+                            <PolarAreaWithoutLegend list={list}></PolarAreaWithoutLegend>
+                            :
+                            <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>
+                        }
+                    </Card>
+
+
+
+                </Box>
+              
+                 <Heading size="xl">
+                    <FontAwesomeIcon icon={faGlobeAmericas} /> TOP 10 CADA SITE
                     </Heading>
-                    {list.length ?
-                        <PolarAreaWithoutLegend list={list}></PolarAreaWithoutLegend>
-                        :
-                        <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>
-                    }
-                </Card>
+                <Divider color="yellow" />
+                <Box>
+                    {group.map((i, c) =>
+                        <Card key={c} className="smallCard"
+                            variant="subtle" color="yellow" m="sm" p="sm">
+                            <div >
+                                <Heading p="sm" size="sm">
+                                    <Text> <FontAwesomeIcon icon={faCube} /> {i.site} </Text>
+                                </Heading>
+                                <Box style={{ padding: "0 5%" }}>
+                                    {
+                                        i.palavras.map((wordCount, count) =>
+                                            <div key={count} className="drac-text drac-text-white">
+                                                <Text> {count + 1}º <b>{wordCount.palavra}
+                                                </b> ({((wordCount.frequencia / i.palavras
+                                                    .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)}%)</Text>
+                                            </div>
+                                        )
+                                    }
+                                </Box>
+                            </div>
+                        </Card>
+                    )}
 
+                </Box>
+                
+            
             </Box>
         </>
     )

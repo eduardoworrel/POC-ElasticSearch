dbaf9a5179a0773f2372b3b9c27dc5baaf807965
eduardoworrel <=>
2022-01-09 22:58:06 -0300

    not log all

--- src/Palavras.Background.ETL/Load/Refinado/load.js
+++ src/Palavras.Background.ETL/Load/Refinado/load.js
@@ -2,10 +2,9 @@ const axios = require('axios')
 require('dotenv').config()
 module.exports = {
     load: async (collection) => {
-        const result = await axios.post('https://palavras-api.eduardoworrel.com/PageWord/StoreWord', {
+        await axios.post('https://palavras-api.eduardoworrel.com/PageWord/StoreWord', {
             Token: process.env.Token,
             Palavras: collection
         })
-        console.log(result.data)
     }
 }

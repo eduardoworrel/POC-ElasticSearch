00888957fb47151277f054cdd5ff5d9abe7006df
eduardoworrel <=>
2022-01-09 22:52:48 -0300

    more 3 sites

--- src/Palavras.Background.ETL/Extract/sitesAction.js
+++ src/Palavras.Background.ETL/Extract/sitesAction.js
@@ -37,6 +37,27 @@ const uol = async (page) => {
         data: pageText
     }
 }
+
+const google = async (page) => {
+    await page.goto("https://news.google.com/topstories?hl=pt-BR&gl=BR");
+    const pageText = await page.evaluate(() => {
+        const _document = document;
+        [
+          ".gb_Fc",
+          ".SbZred",
+          "script",
+          "style"
+        ].forEach((selector) => {
+            _document.querySelector(selector).remove()
+        });
+        return _document.querySelector("body").innerText;
+    });
+
+    return {
+        key: 'news.google.com',
+        data: pageText
+    }
+}
 const cnnbrasil = async (page) => {
     await page.goto("https://www.cnnbrasil.com.br");
     const pageText = await page.evaluate(() => {
@@ -56,53 +77,55 @@ const cnnbrasil = async (page) => {
         data: pageText
     }
 }
-// const jovempan = async (page) => {
-//     await page.goto("https://jovempan.com.br");
-//     const pageText = await page.evaluate(() => {
-//         const _document = document;
-//         [
-//             ".container-top-to-menu-link",
-//             "#footer",
-//             ".container-panflix-collection",
-//             ".podcasts-header-home",
-//             ".container-follow-apps"
+const jovempan = async (page) => {
+    await page.goto("https://jovempan.com.br");
+    const pageText = await page.evaluate(() => {
+        const _document = document;
+        [
+            "#header",
+            "#footer",
+            ".container-panflix-collection",
+            ".podcasts-header-home",
+            ".container-follow-apps"
 
-//         ].forEach((selector) => {
-//             _document.querySelector(selector).remove()
-//         });
-//         return _document.querySelector("body").innerText;
-//     });
+        ].forEach((selector) => {
+            _document.querySelector(selector).remove()
+        });
+        return _document.querySelector("body").innerText;
+    });
 
-//     return {
-//         key: 'jovempan.com.br',
-//         data: pageText
-//     }
-// }
+    return {
+        key: 'jovempan.com.br',
+        data: pageText
+    }
+}
 
-// const terra = async (page) => {
-//     await page.goto("https://www.terra.com.br");
-//     const pageText = await page.evaluate(() => {
-//         const _document = document;
-//             [
-//                 ".table-ad",
-//                 "#zaz-app-t360-navbar",
+const terra = async (page) => {
+    await page.goto("https://www.terra.com.br");
+    const pageText = await page.evaluate(() => {
+        const _document = document;
+            [
+                ".table-ad",
+                "#zaz-app-t360-navbar",
+                ".open-menu-t360"
               
-//             ].forEach((selector) => {
-//                 _document.querySelector(selector).remove()
-//             });;
+            ].forEach((selector) => {
+                _document.querySelector(selector).remove()
+            });
 
-//         return _document.querySelector("body").innerText;
-//     });
+        return _document.querySelector("body").innerText;
+    });
 
-//     return {
-//         key: 'terra.com.br',
-//         data: pageText
-//     }
-// }
+    return {
+        key: 'terra.com.br',
+        data: pageText
+    }
+}
 module.exports = {
     g1,
     uol,
     cnnbrasil,
-    // jovempan,
-    //terra
+    google,
+    jovempan,
+    terra
 }

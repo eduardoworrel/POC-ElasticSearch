c3ae11ab859c7513068a27946f42d481e441537b
eduardoworrel <=>
2022-01-11 20:35:48 -0300

    melhorando usabilidade

--- src/Palavras.Web.SPA/src/Components/Ranking/Ranking.jsx
+++ src/Palavras.Web.SPA/src/Components/Ranking/Ranking.jsx
@@ -1,7 +1,7 @@
 import { Heading, Box, Button, Card, Text, Paragraph, Divider } from "@dracula/dracula-ui";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobeAmericas, faCube, faFilter, faLock } from '@fortawesome/free-solid-svg-icons'
+import { faGlobeAmericas, faCube, faFilter, faLock, faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
 import { Puff } from 'react-loading-icons'
 import { PolarAreaWithoutLegend } from "../Chart/PolarAreaWithoutLegend";
 
@@ -17,6 +17,10 @@ const Ranking = () => {
     const [group, setGroup] = useState([]);
     const [atualizacao, setAtualizacao] = useState("");
     const [classes, setClasses] = useState([]);
+
+    const [filterIsVisible, setFilterIsVisible] = useState(false);
+    const [topIsVisible, setTopIsVisible] = useState(true);
+    const [groupIsVisible, setGroupIsVisible] = useState(false);
 
     const load = () => {
         fetch(UrlRank).then(a => a.json()).then((a) => {
@@ -47,90 +51,119 @@ const Ranking = () => {
                 </Box>
 
 
-                <Heading size="xl">
+                <Heading size="md" onClick={() => { setFilterIsVisible(!filterIsVisible) }}>
                     <FontAwesomeIcon icon={faFilter} /> CLASSES GRAMATICAIS
+                    <span style={{ float: "right", fontSize: "2em", marginRight: "30px" }}>
+                        {
+                            filterIsVisible
+                                ? <FontAwesomeIcon icon={faCaretUp} />
+                                : <FontAwesomeIcon icon={faCaretDown} />}
+
+                    </span>
                 </Heading>
                 <Divider color="purple" />
+                {
+                    filterIsVisible ?
+                        <Box style={{ display: "flex", flexFlow: "row wrap", width: "95%", margin: "0 auto" }}>
+                            <>
+                                {classes.length > 0 ?
+                                    classes.map((classe, k) =>
+                                        <>
+                                            {classe.classe == "?" ?
+                                                <Box style={{ order: "1", margin: "auto" }} key={k} m="">
+                                                    <Button style={{
+                                                        fontSize: ".7em",
+                                                        padding: "0 5px",
+                                                        margin: "3px"
+                                                    }} m="xxs" color="purple" disabled={true} >
+                                                        DESCONHECIDO  <FontAwesomeIcon icon={faLock} />  <span style={{ fontSize: ".6em", background: "#1D0E29", padding: "2px 9px", borderRadius: "5px", color: "white" }}>{classe.quantidade}</span>
+                                                    </Button>
+                                                </Box>
+                                                :
+                                                <Box style={{ order: "2", margin: "auto", }} key={k} m="">
+                                                    <Button style={{
+                                                        fontSize: ".7em",
+                                                        padding: "0 5px",
+                                                        margin: "3px"
+                                                    }} color="purple" variant="ghost">
 
-                <Box style={{ display: "flex", flexFlow: "row wrap", width: "95%", margin: "0 auto" }}>
-                    <>
-                        {classes.length > 0 ?
-                            classes.map((classe, k) =>
-                                <>
-                                    {classe.classe == "?" ?
-                                        <Box style={{ order: "1", margin: "auto" }} key={k} m="">
-                                            <Button m="xxs" color="purple" disabled={true} >
-                                                DESCONHECIDO  <FontAwesomeIcon icon={faLock} />  <span style={{ background: "#1D0E29", padding: "2px 9px", borderRadius: "15px", color: "white" }}>{classe.quantidade}</span>
-                                            </Button>
-                                        </Box>
-                                        :
-                                        <Box style={{ order: "2", margin: "auto" }} key={k} m="">
-                                            <Button m="xxs" color="purple" variant="ghost">
-                                                {classe.classe} <span style={{ background: "#1D0E29", padding: "2px 9px", borderRadius: "15px", color: "white" }}>{classe.quantidade}</span>
-                                            </Button>
-                                        </Box>
-                                    }
-                                </>
-                            )
-                            :
-                            <><Puff stroke="cyan" strokeOpacity={.925} speed={.75} /></>
-                        }
-                    </>
-                </Box>
+                                                        {classe.classe} <span style={{ fontSize: ".6em", background: "#1D0E29", padding: "2px 9px", borderRadius: "5px", color: "bisque" }}>
+                                                            {classe.quantidade}
+                                                        </span>
+                                                    </Button>
+                                                </Box>
+                                            }
+                                        </>
+                                    )
+                                    :
+                                    <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>
+                                }
+                            </>
+                        </Box>
+                        : <></>
+                }
 
-                <Heading size="xl" style={{ marginTop: "75px" }}>
+                <Heading size="lg" style={{ marginTop: "75px" }}
+                    onClick={() => { setTopIsVisible(!topIsVisible) }}>
                     <FontAwesomeIcon icon={faGlobeAmericas} /> TOP 10
+                    <span style={{ float: "right", fontSize: "2em", marginRight: "30px" }}>
+                        {
+                            topIsVisible
+                                ? <FontAwesomeIcon icon={faCaretUp} />
+                                : <FontAwesomeIcon icon={faCaretDown} />}
+
+                    </span>
                 </Heading>
                 <Divider color="pink" />
-                <Box style={{ display: "flex", flexFlow: "row wrap" }}>
-                    <Card style={{ flex: "1", minWidth: "240px" }}
-                        variant="subtle"
-                        color="pink" m="xs" p="sm">
-
-                        <div>
-                            <Heading p="sm" size="sm">
-                                <Text color="white">Ultima Atualização</Text>
-                            </Heading>
+                {topIsVisible ?
+                    <Box style={{ display: "flex", flexFlow: "row wrap" }}>
+                        <Card style={{ flex: "1", minWidth: "240px" }}
+                            variant="subtle"
+                            color="pink" m="xs" p="sm">
 
                             <Box style={{ padding: "0 7%" }}>
                                 {atualizacao ?
                                     <Paragraph >
-                                        <Text color="white"> <b style={{fontSize:"1.5em"}} >{atualizacao}</b> </Text>
+
+                                        <Text color="white"> Ultima Atualização <b style={{ fontSize: "1.5em" }} >{atualizacao}</b> </Text>
                                     </Paragraph>
                                     : <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>}
                             </Box>
-                        </div>
-                    </Card>
-                    {list.length ?
-                        <>
-                            {list.map((i, c) =>
-                                <>
-                                    <Box key={c} style={{ flex: "1", }} >
-                                        <Card style={{ minWidth: "240px" }} variant="subtle" color="pink" m="xs" p="sm">
-                                            <Box>
-                                                <Text>
-                                                    <div> {c + 1}º <b>{i.palavra}</b></div>
-                                                    <br />
-                                                    <div style={{ background: "#4C1A2D", padding: "6px 8px 7px 12px", borderRadius: "8px", color: "bisque" }}>
-                                                        <b>{((i.frequencia / list
-                                                            .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)}%</b> dos top 10
-                                                    </div>
-                                                    <div style={{ padding: "6px 8px 7px 12px", color: "bisque" }}>
-                                                        apareceu <b>{i.frequencia}</b> vezes
-                                                    </div>
-                                                </Text>
-                                            </Box>
-                                        </Card>
-                                    </Box>
-                                </>
-                            )}
-                        </>
-                        :
-                        <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>
-                    }
+
+                        </Card>
+                        {list.length ?
+                            <>
+                                {list.map((i, c) =>
+                                    <>
+                                        <Box key={c} style={{ flex: "1", }} >
+                                            <Card style={{ minWidth: "240px" }} variant="subtle" color="pink" m="xs" p="sm">
+                                                <Box>
+                                                    <Text>
+                                                        <div> {c + 1}º <b>{i.palavra}</b></div>
+                                                        <br />
+                                                        <div style={{ background: "#4C1A2D", padding: "6px 8px 7px 12px", borderRadius: "8px", color: "white" }}>
+                                                            <b>{((i.frequencia / list
+                                                                .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)}%</b> dos top 10
+                                                </div>
+                                                        <div style={{ padding: "6px 8px 7px 12px", color: "white" }}>
+                                                            apareceu <b>{i.frequencia}</b> vezes
+                                                </div>
+                                                    </Text>
+                                                </Box>
+                                            </Card>
+                                        </Box>
+                                    </>
+                                )}
+                            </>
+                            :
+                            <><Puff stroke="pink" strokeOpacity={.925} speed={.75} /></>
+                        }
 
 
-                </Box>
+                    </Box>
+                    :
+                    <></>}
+
 
                 {/* <Card style={{ flex: "3", }}
                         variant="subtle" color="pink" m="xs" >
@@ -146,41 +179,53 @@ const Ranking = () => {
 
 
 
-                <Heading size="xl" style={{ marginTop: "75px" }}>
+                <Heading size="md" style={{ marginTop: "75px" }}
+                    onClick={() => { setGroupIsVisible(!groupIsVisible) }}>
                     <FontAwesomeIcon icon={faGlobeAmericas} /> TOP 10 CADA SITE
-                    </Heading>
+                    <span style={{ float: "right", fontSize: "2em", marginRight: "30px" }}>
+                        {
+                            groupIsVisible
+                                ? <FontAwesomeIcon icon={faCaretUp} />
+                                : <FontAwesomeIcon icon={faCaretDown} />}
+
+                    </span>
+                </Heading>
                 <Divider color="yellow" />
+                {groupIsVisible ?
                 <Box style={{ display: "flex", flexFlow: "row wrap" }}>
-                    {group.map((i, c) =>
-                        <Box key={c} style={{ flex: "2", minWidth: "240px" }}>
-                            <div >
-                                <Heading p="sm" size="sm">
-                                    <Text> <FontAwesomeIcon icon={faCube} /> {i.site} </Text>
-                                </Heading>
-                                <Box style={{ padding: "0 0%" }}>
-                                    {
-                                        i.palavras.map((wordCount, count) =>
-                                            <Card key={count} variant="subtle" color="yellow" p="sm" m="sm">
-                                                <Text>
-                                                    {count + 1}º <b>{wordCount.palavra}<br /></b>
-                                                </Text>
-                                                <div style={{ padding: "6px 0px 0px 0px", color: "bisque" }}>
-                                                    <b>{((wordCount.frequencia / i.palavras
-                                                        .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)}%</b> dos top 10
-                                                    </div>
-                                                <div style={{padding: "6px 0px 0px 0px", color: "bisque" }}>
-                                                    apareceu <b>{wordCount.frequencia}</b> vezes
+                {group.map((i, c) =>
+                    <Box key={c} style={{ flex: "2", minWidth: "240px" }}>
+                        <div >
+                            <Heading p="sm" size="sm">
+                                <Text> <FontAwesomeIcon icon={faCube} /> {i.site} </Text>
+                            </Heading>
+                            <Box style={{ padding: "0 0%" }}>
+                                {
+                                    i.palavras.map((wordCount, count) =>
+                                        <Card key={count} variant="subtle" color="yellow" p="sm" m="sm">
+                                            <Text>
+                                                {count + 1}º <b>{wordCount.palavra}<br /></b>
+                                            </Text>
+                                            <div style={{ padding: "6px 0px 0px 0px", color: "bisque" }}>
+                                                <b>{((wordCount.frequencia / i.palavras
+                                                    .reduce((a, b) => a + b.frequencia, 0)) * 100).toFixed(2)}%</b> dos top 10
                                                 </div>
-                                            </Card>
+                                            <div style={{ padding: "6px 0px 0px 0px", color: "bisque" }}>
+                                                apareceu <b>{wordCount.frequencia}</b> vezes
+                                            </div>
+                                        </Card>
 
-                                        )
-                                    }
-                                </Box>
-                            </div>
-                        </Box>
-                    )}
+                                    )
+                                }
+                            </Box>
+                        </div>
+                    </Box>
+                )}
 
-                </Box>
+            </Box>
+                :
+                <></>}
+                
 
 
             </Box >
